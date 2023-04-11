011edd100b03e48fb65a197e5b1dbb4a83207843
leojomar01 <leojomar01@gmail.com>
2023-04-11 02:20:39 -0700

    1st commit

---
+++ js/index.js
@@ -0,0 +1,72 @@
+// switch event listener
+$('.menu').click(function (e) { 
+    $('.menuItems').toggleClass('active'); 
+    $(this).toggleClass('active'); 
+});
+
+$('.darkMode').click(function(){
+  $(this).toggleClass('active');
+
+  if($(this).hasClass('active')){
+    $(":root").css({
+      '--bg': '#494949',
+      '--font': '#e9e9e9',
+      '--navfont': '#494949',
+      '--navfonthover': '#bdbdbd',
+      '--nav': '#ffffff'
+    });
+  }
+  else{
+    $(":root").css({
+      '--bg': '#e9e9e9',
+      '--font': '#000000',
+      '--navfont': '#ffffff',
+      '--navfonthover': '#bdbdbd',
+      '--nav': '#494949'
+    });
+  }
+})
+
+// Navbar auto hide/show
+var prevScrollpos = window.pageYOffset;
+$(document).scroll(function () { 
+    var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    $('nav').css('top',"0")
+  } else {
+    $('nav').css('top',"-12vh")
+  }
+  $('.menuItems').removeClass('active');  
+  $('.menu').removeClass('active');  
+  selectActive();
+  prevScrollpos = currentScrollPos;
+});
+
+// skills
+$('.skill .1').text('Basic')
+$('.skill .2').text('Intermediate')
+$('.skill .3').text('Advanced')
+$('.skill .4').text('Expert')
+
+
+
+let sections = document.querySelectorAll('.section');
+let links = document.querySelectorAll('.menuItems li a');
+
+function selectActive(){
+  let scrollY = window.pageYOffset;
+  sections.forEach((section,i) =>{
+    let height = section.offsetHeight
+    let top = section.offsetTop - 200
+      if(scrollY > top && scrollY <=top + height){
+        section.classList.add('active')
+        links[i].classList.add('active-link')
+      }
+      else{
+        links[i].classList.remove('active-link')
+      }
+  })
+}
+
+
+
